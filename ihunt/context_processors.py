b979837a62d84a698e12a0fc43053b261105e03b
Richard Connon <richard@connon.me.uk>
2015-09-20 18:49:35 +0100

    Handle the case where a user has no profile in the event_team context processor

--- ihunt/context_processors.py
+++ ihunt/context_processors.py
@@ -1,10 +1,14 @@
 from ihunt.utils import with_event
+from ihunt.models import UserProfile
 
 
 @with_event
 def event_team(request, event):
-    user = request.user.profile
-    team = user.teams.get(at_event=event)
+    try:
+        user = request.user.profile
+        team = user.teams.get(at_event=event)
+    except UserProfile.DoesNotExist:
+        team = None
 
     return {
         'team': team
