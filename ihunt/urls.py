01f25b6704847415666c5bc8cb451f4dc47f0347
Richard Connon <richard@connon.me.uk>
2016-03-02 18:25:58 +0000

    Formatting in urls.py

--- ihunt/urls.py
+++ ihunt/urls.py
@@ -11,9 +11,21 @@ eventpatterns = [
 
 urlpatterns = [
     url(r'^event/(?P<event_id>[0-9]+)/', include(eventpatterns)),
-    url(r'^$', TemplateView.as_view(template_name="ihunt/index.html"), name='index'),
-    url(r'^faq$', TemplateView.as_view(template_name="ihunt/faq.html"), name='faq'),
-    url(r'^help$', TemplateView.as_view(template_name="ihunt/help.html"), name='help'),
+    url(
+        r'^$',
+        TemplateView.as_view(template_name="ihunt/index.html"),
+        name='index'
+    ),
+    url(
+        r'^faq$',
+        TemplateView.as_view(template_name="ihunt/faq.html"),
+        name='faq'
+    ),
+    url(
+        r'^help$',
+        TemplateView.as_view(template_name="ihunt/help.html"),
+        name='help'
+    ),
     url(r'^login$', ihunt.views.login_view, name='login'),
     url(r'^logout$', logout, name='logout'),
     url(r'', include(eventpatterns)),
