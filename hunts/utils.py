0fbfbaace1027c4cd342b13e85ea9a6f6a11cbd3
Richard Connon <richard@connon.me.uk>
2017-03-08 20:30:31 +0000

    Raise 404 for missing episodes

--- hunts/utils.py
+++ hunts/utils.py
@@ -1,3 +1,6 @@
+from django.http import Http404
+from .models import Episode
+
 def current_puzzle(puzzlesets, team):
     for cs in puzzlesets:
         for c in cs.puzzles.all():
@@ -9,4 +12,7 @@ def current_puzzle(puzzlesets, team):
 def event_episode(event, episode_number):
     episodes = event.episodes.order_by('start_date')
     ep_int = int(episode_number)
-    return episodes[ep_int - 1:ep_int].get()
+    try:
+        return episodes[ep_int - 1:ep_int].get()
+    except Episode.DoesNotExist as e:
+        raise Http404 from e
