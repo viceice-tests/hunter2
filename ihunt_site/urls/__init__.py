7c74d0a71f4fcbe30a07cdd45c33721244d40c27
Richard Connon <richard@connon.me.uk>
2017-03-20 19:35:31 +0000

    Feeble attempt at redirecting if an unrecognised name is used

--- ihunt_site/urls/__init__.py
+++ ihunt_site/urls/__init__.py
@@ -1,1 +1,7 @@
-urlpatterns = []
+from django.conf.urls import url
+from django.contrib.sites.models import Site
+from django.views.generic.base import RedirectView
+
+urlpatterns = [
+    url(r'^', RedirectView.as_view(url=f'//www.{Site.objects.get_current().domain}'))
+]
