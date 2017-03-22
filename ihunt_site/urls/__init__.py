84759ab733e92291aa8f11cca5206428b4f5d7e6
Richard Connon <richard@connon.me.uk>
2017-03-22 15:51:40 +0000

    Redirecting using Site breaks migration

--- ihunt_site/urls/__init__.py
+++ ihunt_site/urls/__init__.py
@@ -1,7 +1,1 @@
-from django.conf.urls import url
-from django.contrib.sites.models import Site
-from django.views.generic.base import RedirectView
-
-urlpatterns = [
-    url(r'^', RedirectView.as_view(url=f'//www.{Site.objects.get_current().domain}'))
-]
+urlpatterns = []
