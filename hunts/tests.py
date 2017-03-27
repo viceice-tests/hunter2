4730e90137ca9d9a20c9e9f8cc89d6bb0af572a8
Richard Connon <richard@connon.me.uk>
2017-03-28 00:48:09 +0100

    Broken tests

--- hunts/tests.py
+++ hunts/tests.py
@@ -1,7 +1,10 @@
 from django.contrib.auth.models import User
+from django.contrib.sites.models import Site
 from django.test import TestCase
-from teams.models import UserProfile
-from .models import Answer, Guess, Puzzle
+from django.utils import timezone
+from events.models import Event, Theme
+from teams.models import Team, UserProfile
+from .models import Answer, Episode, Guess, Puzzle, TeamPuzzleData
 from . import runtime
 
 
@@ -86,3 +89,33 @@ class LuaAnswerValidationTests(TestCase):
         self.assertFalse(guess.is_right())
         guess = Guess.objects.filter(guess='wrong').get()
         self.assertFalse(guess.is_right())
+
+
+class PuzzleStartTimeTests(TestCase):
+    def setUp(self):
+        Site.objects.create(domain='localhost', name='localhost')
+        test_name = self.__class__.__name__
+        theme = Theme.objects.create(name=test_name)
+        event = Event.objects.create(name=test_name, theme=theme)
+        puzzle = Puzzle.objects.create(title=test_name, content=test_name)
+        episode = Episode.objects.create(
+            name=test_name,
+            start_date=timezone.now(),
+            event=event
+        )
+        episode.puzzles.add(puzzle)
+        team = Team.objects.create(at_event=event)
+        user = User.objects.create(username='user', password='hunter2')
+        profile = UserProfile.objects.create(user=user)
+        profile.teams.add(team)
+
+    def test_start_times(self):
+        self.assertTrue(self.client.login(username='user', password='hunter2'))
+        self.assertEquals(response.status_code, 200)
+        response = self.client.get('/ep/1/pz/1')
+        self.assertEquals(response.status_code, 200)
+        first_time = TeamPuzzleData.objects.get().start_time
+        self.assertIsNot(first_time, None)
+        self.client.get('/ep/1/pz/1')
+        second_time = TeamPuzzleData.objects.get().start_time
+        self.assertEqual(first_time, second_time)
