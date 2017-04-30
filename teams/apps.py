f1e08031ad6870702987407107a2d5c10acbf822
Richard Connon <richard@connon.me.uk>
2017-05-01 00:15:14 +0100

    Remove debug logging

--- teams/apps.py
+++ teams/apps.py
@@ -1,12 +1,9 @@
 from django.apps import AppConfig
 
-import logging
-
 
 class TeamsConfig(AppConfig):
     name = 'teams'
 
     def ready(self):
-        logging.error('Init bruv')
         super(TeamsConfig, self).ready()
         from . import signals
