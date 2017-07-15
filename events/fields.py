be7d452a9c36db06f68a7edad712d55132475df6
Robert Blenkinsopp <robert@blenkinsopp.net>
2017-07-15 14:45:26 +0100

    Constain a single event to be current
    This is done by creating a new field type which only allows a single
    value to True.

---
+++ events/fields.py
@@ -0,0 +1,17 @@
+from django.db.models import BooleanField
+
+
+class SingleTrueBooleanField(BooleanField):
+
+    def pre_save(self, model_instance, add):
+        objects = model_instance.__class__.objects
+
+        # Ensure all others are false if this value is True
+        if getattr(model_instance, self.attname):
+            objects.update(**{self.attname: False})
+
+        # If none is set as true, ensure this one is set as True
+        elif not objects.exclude(id=model_instance.id).filter(**{self.attname: True}):
+            return True
+
+        return getattr(model_instance, self.attname)
