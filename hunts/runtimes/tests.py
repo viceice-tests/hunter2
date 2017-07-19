1bf3766d0baf2d99671331c9c5be502c0b755ac9
Richard Connon <richard@connon.me.uk>
2017-07-19 21:38:57 +0100

    Test IFrameRuntime raises an exception for validate_guess

--- hunts/runtimes/tests.py
+++ hunts/runtimes/tests.py
@@ -5,22 +5,14 @@ from .regex import RegexRuntime
 from .static import StaticRuntime
 
 
-class StaticRuntimeTestCase(TestCase):
-    def test_evaluate(self):
-        static_runtime = StaticRuntime()
-        static_script = '''Hello  World!'''
-        result = static_runtime.evaluate(static_script, None, None, None, None)
-        self.assertEqual(result, static_script)
+class IFrameRuntimeTestCase(TestCase):
+    def setUp(self):
+        self.iframe_runtime = IFrameRuntime()
 
     def test_validate_guess(self):
-        static_runtime = StaticRuntime()
-        static_script = '''answer'''
-        guess1 = "answer"
-        result = static_runtime.validate_guess(static_script, guess1, None, None)
-        self.assertTrue(result)
-        guess2 = "incorrect answer"
-        result = static_runtime.validate_guess(static_script, guess2, None, None)
-        self.assertFalse(result)
+        iframe_url = r'https://example.com/'
+        with self.assertRaises(NotImplementedError):
+            self.iframe_runtime.validate_guess(iframe_url, "", None, None, None, None)
 
 
 class RegexRuntimeTestCase(TestCase):
@@ -45,3 +37,21 @@ class RegexRuntimeTestCase(TestCase):
         regex_script = r'[]'
         with self.assertRaises(SyntaxError):
             regex_runtime.validate_guess(regex_script, "", None, None)
+
+
+class StaticRuntimeTestCase(TestCase):
+    def test_evaluate(self):
+        static_runtime = StaticRuntime()
+        static_script = '''Hello  World!'''
+        result = static_runtime.evaluate(static_script, None, None, None, None)
+        self.assertEqual(result, static_script)
+
+    def test_validate_guess(self):
+        static_runtime = StaticRuntime()
+        static_script = '''answer'''
+        guess1 = "answer"
+        result = static_runtime.validate_guess(static_script, guess1, None, None)
+        self.assertTrue(result)
+        guess2 = "incorrect answer"
+        result = static_runtime.validate_guess(static_script, guess2, None, None)
+        self.assertFalse(result)
