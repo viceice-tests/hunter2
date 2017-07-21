e0cfe1d60545300fb6fef959255b4eaafa606f23
Richard Connon <richard@connon.me.uk>
2017-07-21 14:47:15 +0100

    Rename signal handler to avoid conflict with #57

--- hunts/signals.py
+++ hunts/signals.py
@@ -5,7 +5,7 @@ from . import models
 
 
 @receiver(m2m_changed, sender=models.Episode.prequels.through)
-def members_changed(sender, instance, action, pk_set, **kwargs):
+def episode_prequels_changed(sender, instance, action, pk_set, **kwargs):
     if action == 'pre_add':
         for episode_id in pk_set:
             episode = models.Episode.objects.get(pk=episode_id)
