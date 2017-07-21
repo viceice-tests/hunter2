7ebc4b49f4adc9520cd64972a1bc7c06f3a780fc
Richard Connon <richard@connon.me.uk>
2017-07-21 11:17:38 +0100

    Enable django_extensions app

--- hunter2/settings.py
+++ hunter2/settings.py
@@ -60,6 +60,7 @@ INSTALLED_APPS = (
     'allauth.socialaccount',
     'allauth.socialaccount.providers.openid',
     'debug_toolbar',
+    'django_extensions',
     'nested_admin',
     'rules.apps.AutodiscoverRulesConfig',
     'sortedm2m',
