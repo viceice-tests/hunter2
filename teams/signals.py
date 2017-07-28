07227e544ce1e08835f6aea1b9005a821e6640c8
Chris Le Sueur <thefishface+gitlab@gmail.com>
2017-07-28 18:07:49 +0100

    Implement maximum team size. Validation is fuuugly

--- teams/signals.py
+++ teams/signals.py
@@ -7,6 +7,9 @@ from . import models
 @receiver(m2m_changed, sender=models.Team.members.through)
 def members_changed(sender, instance, action, pk_set, **kwargs):
     if action == 'pre_add':
+        max_size = instance.at_event.max_team_size
+        if len(pk_set) > max_size > 0:
+            raise ValidationError('Teams can have at most %d members' % max_size)
         for user_id in pk_set:
             user = models.UserProfile.objects.get(pk=user_id)
             if models.Team.objects.exclude(pk=instance.pk).filter(at_event=instance.at_event).filter(members=user).count() > 0:
