968e1ed18421b06c5df86ed646f909c1e9a94d48
Chris Le Sueur <thefishface+gitlab@gmail.com>
2017-08-01 23:34:30 +0100

    Add seat field

--- teams/models.py
+++ teams/models.py
@@ -8,6 +8,7 @@ import events
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    seat = models.CharField(max_length=6, blank=True, default='')
 
     def __str__(self):
         return f'<UserProfile: {self.user.username}>'
