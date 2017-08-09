82d4e2b90d077e88abccb769b0dd71cc8f1e74e0
Robert Blenkinsopp <robert@blenkinsopp.net>
2017-08-09 16:54:28 +0100

    Link events app configuration correctly

--- events/apps.py
+++ events/apps.py
@@ -3,3 +3,7 @@ from django.apps import AppConfig
 
 class EventsConfig(AppConfig):
     name = 'events'
+
+    def ready(self):
+        super(EventsConfig, self).ready()
+        from . import signals  # noqa: F401
