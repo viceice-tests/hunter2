4f7a43d9363b1c235a713d10297407522258a967
Richard Connon <richard@connon.me.uk>
2017-09-23 10:44:01 +0100

    Revert "Printing 'waiting for database connection' breaks commands with machine readable output"
    This reverts commit cfcac2d46e486cb9b6c83309ee55b2fe67f1fb2b.

--- manage.py
+++ manage.py
@@ -15,6 +15,7 @@ if __name__ == "__main__":
     # Wait for a connection to the database to become available
     from hunter2.settings import DATABASES
     import wait
+    print("Waiting for database connection...")
     DB_HOST = DATABASES['default']['HOST']
     DB_PORT = DATABASES['default']['PORT']
     if not wait.tcp.open(DB_PORT, host=DB_HOST, timeout=10):  # nocover
