93de9136348129de6b85805da943b3ecc0a54fff
Chris Le Sueur <thefishface+gitlab@gmail.com>
2018-04-07 00:17:32 +0100

    jshintage

--- hunts/static/hunts/js/guesses.js
+++ hunts/static/hunts/js/guesses.js
@@ -10,11 +10,12 @@ function updateGuesses(force) {
  * https://stackoverflow.com/questions/19491336/get-url-parameter-jquery-or-how-to-get-query-string-values-in-js/21903119
  */
 function getQueryParam(param) {
+	"use strict";
 	location.search.substr(1)
 		.split("&")
 		.some(function(item) { // returns first occurence and stops
-			return item.split("=")[0] == param && (param = item.split("=")[1])
-		})
+			return item.split("=")[0] == param && (param = item.split("=")[1]);
+		});
 	return param;
 }
 
