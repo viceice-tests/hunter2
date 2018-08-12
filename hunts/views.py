d8ddbf98331d10cd353ef7fd24a74ca86c9398a0
Richard Connon <richard@connon.me.uk>
2018-08-12 21:57:26 +0100

    Annotate seat information (using a helper function) on other userprofile querysets

--- hunts/views.py
+++ hunts/views.py
@@ -34,6 +34,7 @@ from teams.mixins import TeamMixin
 from . import models, rules, runtimes, utils
 from .mixins import EpisodeUnlockedMixin, PuzzleUnlockedMixin
 from events.models import Attendance
+from events.utils import annotate_userprofile_queryset_with_seat
 
 import hunter2
 import teams
@@ -158,6 +159,10 @@ class GuessesContent(LoginRequiredMixin, View):
             'by_team__id', 'by_team__name',
             'by__user__id', 'by__user__username',
             'correct_for__id'
+        ).annotate(
+            by__user__seat=Subquery(
+                Attendance.objects.filter(user=OuterRef('pk'), event=self.request.tenant).values('seat')
+            )
         ).prefetch_related(
             Prefetch(
                 'for_puzzle__episode_set',
@@ -632,8 +637,10 @@ class AboutView(TemplateView):
         files = {f.slug: f.file.url for f in self.request.tenant.eventfile_set.all()}
         content = Template(self.request.tenant.about_text).safe_substitute(**files)
 
+        admin_members = annotate_userprofile_queryset_with_seat(admin_team.members, self.request.tenant)
+
         context.update({
-            'admins': admin_team.members.annotate(seat=Subquery(Attendance.objects.filter(user=OuterRef('pk'), event=self.request.tenant).values('seat'))),
+            'admins': admin_members,
             'content': content,
             'event_name': self.request.tenant.name,
         })
