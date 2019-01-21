c76a0c86b6df5bd5cc9426f8bfff9faeb4795c6d
Richard Connon <richard@connon.me.uk>
2019-01-21 21:47:40 +0000

    Remove unused import

--- hunter2/context_processors.py
+++ hunter2/context_processors.py
@@ -10,8 +10,6 @@
 #
 # You should have received a copy of the GNU Affero General Public License along with Hunter2.  If not, see <http://www.gnu.org/licenses/>.
 
-from urllib.parse import urlparse
-
 from django.urls import reverse
 
 from .utils import wwwize
