1771f878f395f03c60fbe7944109240a1817d262
Robert Blenkinsopp <robert@blenkinsopp.net>
2019-02-10 17:22:43 +0000

    Fix JSLint errors

--- hunter2/js/index.js
+++ hunter2/js/index.js
@@ -1,4 +1,4 @@
-import('../static/hunter2/scss/base.scss');
+import '../static/hunter2/scss/base.scss';
 
 var $ = require('jquery');
 
--- postcss.config.js
+++ postcss.config.js
@@ -3,4 +3,5 @@ module.exports = {
     require('precss'),
     require('autoprefixer')
   ]
-}
+};
+
